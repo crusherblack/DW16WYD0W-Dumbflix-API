da319eafe406b7a067fd2074a06aa8d12aa9f2ca
crusherblack <fadhildarma13@gmail.com>
2020-06-15 22:17:44 +0700

    fix pagination

--- server/controllers/film.js
+++ server/controllers/film.js
@@ -1,54 +1,48 @@
-const {
-	Film,
-	Category,
-	Episode
-} = require("../models");
-const Joi = require("@hapi/joi");
-const {
-	response
-} = require("express");
+const { Film, Category, Episode } = require('../models');
+const Joi = require('@hapi/joi');
+const { response } = require('express');
 
 exports.getFilm = async (req, res) => {
 	try {
-		const page = 0;
-		const pageSize = 10;
+		const { page: pageQuery, limit: limitQuery } = req.query;
 
-		const paginate = ({
-			page,
-			pageSize
-		}) => {
+		const page = pageQuery ? parseInt(pageQuery) - 1 : 0;
+		const pageSize = limitQuery ? parseInt(limitQuery) : 10;
+
+		const paginate = ({ page, pageSize }) => {
 			const offset = page * pageSize;
 			const limit = pageSize;
 
 			return {
 				offset,
-				limit,
+				limit
 			};
 		};
 
 		const film = await Film.findAll({
-			include: [{
+			include: [
+				{
 					model: Episode,
-					as: "episodes",
+					as: 'episodes',
 					attributes: {
-						exclude: ["createdAt", "updatedAt", "filmId", "FilmId"],
-					},
+						exclude: [ 'createdAt', 'updatedAt', 'filmId', 'FilmId' ]
+					}
 				},
 				{
 					model: Category,
-					as: "category",
+					as: 'category',
 					attributes: {
-						exclude: ["createdAt", "updatedAt"],
-					},
-				},
+						exclude: [ 'createdAt', 'updatedAt' ]
+					}
+				}
 			],
 			attributes: {
-				exclude: ["createdAt", "updatedAt", "categoryId"]
+				exclude: [ 'createdAt', 'updatedAt', 'categoryId' ]
 			},
 			...paginate({
 				page,
 				pageSize
-			}),
+			})
 		});
 
 		if (film) {
@@ -56,22 +50,21 @@ exports.getFilm = async (req, res) => {
 				data: film,
 				paginationInfo: {
 					currentPage: page + 1,
-					totalData: await Film.count(),
-				},
+					totalData: await Film.count()
+				}
 			});
 		} else {
 			return res.status(400).send({
 				error: {
-					message: "Films Not Found"
+					message: 'Films Not Found'
 				}
-
 			});
 		}
 	} catch (error) {
 		console.log(error);
 		return res.status(500).send({
 			error: {
-				message: "Server Error"
+				message: 'Server Error'
 			}
 		});
 	}
@@ -80,48 +73,47 @@ exports.getFilm = async (req, res) => {
 //gak pake tapi ini adalah hasMany Relationship
 exports.getFilmEpisodeCategory = async (req, res) => {
 	try {
-		const {
-			id: IdFilm
-		} = req.params;
+		const { id: IdFilm } = req.params;
 
 		const film = await Film.findAll({
-			include: [{
+			include: [
+				{
 					model: Episode,
-					as: "episodes",
+					as: 'episodes',
 					attributes: {
-						exclude: ["createdAt", "updatedAt"],
-					},
+						exclude: [ 'createdAt', 'updatedAt' ]
+					}
 				},
 				{
 					model: Category,
-					as: "category",
+					as: 'category',
 					attributes: {
-						exclude: ["createdAt", "updatedAt"],
-					},
-				},
+						exclude: [ 'createdAt', 'updatedAt' ]
+					}
+				}
 			],
 
 			attributes: {
-				exclude: ["createdAt", "updatedAt", "categoryId"],
-			},
+				exclude: [ 'createdAt', 'updatedAt', 'categoryId' ]
+			}
 		});
 
 		//return console.log(film);
 
 		if (film) {
 			return res.send({
-				data: film,
+				data: film
 			});
 		} else {
 			return res.status(400).send({
-				message: "Films Not Found"
+				message: 'Films Not Found'
 			});
 		}
 	} catch (error) {
 		console.log(error);
 		return res.status(500).send({
 			error: {
-				message: "Server Error"
+				message: 'Server Error'
 			}
 		});
 	}
@@ -129,39 +121,37 @@ exports.getFilmEpisodeCategory = async (req, res) => {
 
 exports.getDetailFilm = async (req, res) => {
 	try {
-		const {
-			id
-		} = req.params;
+		const { id } = req.params;
 		const film = await Film.findOne({
 			where: {
-				id,
+				id
 			},
 			include: {
 				model: Category,
-				as: "category",
+				as: 'category',
 				attributes: {
-					exclude: ["createdAt", "updatedAt"],
-				},
+					exclude: [ 'createdAt', 'updatedAt' ]
+				}
 			},
 			attributes: {
-				exclude: ["createdAt", "updatedAt", "categoryId"]
-			},
+				exclude: [ 'createdAt', 'updatedAt', 'categoryId' ]
+			}
 		});
 
 		if (film) {
 			return res.send({
-				data: film,
+				data: film
 			});
 		} else {
 			return res.status(400).send({
-				message: "Films Not Found"
+				message: 'Films Not Found'
 			});
 		}
 	} catch (error) {
 		console.log(error);
 		return res.status(500).send({
 			error: {
-				message: "Server Error"
+				message: 'Server Error'
 			}
 		});
 	}
@@ -174,69 +164,65 @@ exports.addFilm = async (req, res) => {
 			thumbnailFilm: Joi.string().required(),
 			year: Joi.number().integer().min(1900).max(2020).required(),
 			category: Joi.required(),
-			description: Joi.string().min(10).required(),
+			description: Joi.string().min(10).required()
 		});
-		const {
-			error
-		} = schema.validate(req.body);
+		const { error } = schema.validate(req.body);
 
 		if (error)
 			return res.status(400).send({
 				error: {
-					message: error.details[0].message,
-				},
+					message: error.details[0].message
+				}
 			});
 
-		const {
-			category
-		} = req.body;
+		const { category } = req.body;
 
 		const cekCategory = await Category.findOne({
 			where: {
-				id: category.id,
-			},
+				id: category.id
+			}
 		});
 
 		if (!cekCategory)
 			return res.status(400).send({
-				message: "Category Not Found"
+				message: 'Category Not Found'
 			});
 
 		const film = await Film.create({
 			...req.body,
-			categoryId: category.id,
+			categoryId: category.id
 		});
 
 		if (film) {
 			const filmResult = await Film.findOne({
 				where: {
-					id: film.id,
+					id: film.id
 				},
 				include: {
 					model: Category,
-					as: "category",
+					as: 'category',
 					attributes: {
-						exclude: ["createdAt", "updatedAt"],
-					},
+						exclude: [ 'createdAt', 'updatedAt' ]
+					}
 				},
 				attributes: {
-					exclude: ["createdAt", "updatedAt"]
-				},
+					exclude: [ 'createdAt', 'updatedAt' ]
+				}
 			});
 
 			return res.send({
-				data: filmResult,
+				data: filmResult
 			});
 		} else {
 			return res.status(400).send({
-				message: "Please Try Again"
+				message: 'Please Try Again'
 			});
 		}
 	} catch (error) {
 		console.log(error);
 		return res.status(500).send({
 			error: {
-				message: "Server Error"
+				message: 'Server Error'
 			}
 		});
 	}
@@ -249,67 +235,62 @@ exports.editFilm = async (req, res) => {
 			thumbnailFilm: Joi.string().required(),
 			year: Joi.required(),
 			category: Joi.required(),
-			description: Joi.string().min(10).required(),
+			description: Joi.string().min(10).required()
 		});
-		const {
-			error
-		} = schema.validate(req.body);
+		const { error } = schema.validate(req.body);
 
 		if (error)
 			return res.status(400).send({
 				error: {
-					message: error.details[0].message,
-				},
+					message: error.details[0].message
+				}
 			});
 
-		const {
-			id
-		} = req.params;
+		const { id } = req.params;
 
-		const {
-			category: {
-				id: categoryId
-			},
-		} = req.body;
+		const { category: { id: categoryId } } = req.body;
 
-		const film = await Film.update({
-			...req.body,
-			categoryId,
-		}, {
-			where: {
-				id,
+		const film = await Film.update(
+			{
+				...req.body,
+				categoryId
 			},
-		});
+			{
+				where: {
+					id
+				}
+			}
+		);
 
 		if (film) {
 			const filmResult = await Film.findOne({
 				where: {
-					id,
+					id
 				},
 				include: {
 					model: Category,
-					as: "category",
+					as: 'category',
 					attributes: {
-						exclude: ["createdAt", "updatedAt"],
-					},
+						exclude: [ 'createdAt', 'updatedAt' ]
+					}
 				},
 				attributes: {
-					exclude: ["createdAt", "updatedAt", "categoryId"]
-				},
+					exclude: [ 'createdAt', 'updatedAt', 'categoryId' ]
+				}
 			});
 			return res.send({
-				data: filmResult,
+				data: filmResult
 			});
 		} else {
 			return res.status(400).send({
-				message: "Films Not Found"
+				message: 'Films Not Found'
 			});
 		}
 	} catch (error) {
 		console.log(error);
 		return res.status(500).send({
 			error: {
-				message: "Server Error"
+				message: 'Server Error'
 			}
 		});
 	}
@@ -317,37 +298,35 @@ exports.editFilm = async (req, res) => {
 
 exports.deleteFilm = async (req, res) => {
 	try {
-		const {
-			id
-		} = req.params;
+		const { id } = req.params;
 		const film = await Film.findOne({
 			where: {
-				id,
-			},
+				id
+			}
 		});
 
 		if (film) {
 			await Film.destroy({
 				where: {
-					id,
-				},
+					id
+				}
 			});
 
 			return res.send({
 				data: {
-					id,
-				},
+					id
+				}
 			});
 		} else {
 			return res.status(400).send({
-				message: "Film Not Found"
+				message: 'Film Not Found'
 			});
 		}
 	} catch (error) {
 		console.log(error);
 		return res.status(500).send({
 			error: {
-				message: "Server Error"
+				message: 'Server Error'
 			}
 		});
 	}
